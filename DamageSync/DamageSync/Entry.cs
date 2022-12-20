062a6dbf3972a40c27ef7240281cdeed8e09dc3b
randomuserhi <magicalworld.nano@gmail.com>
2022-12-20 00:46:15 +0000

    Initial commit

---
+++ DamageSync/DamageSync/Entry.cs
@@ -0,0 +1,32 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using BepInEx.Unity.IL2CPP;
+using HarmonyLib;
+using UnityEngine;
+
+using API;
+using DamageSync.Patches;
+using Player;
+
+namespace DamageSync
+{
+    public static class Module
+    {
+        public const string GUID = "randomuserhi.DamageSync";
+        public const string Name = "DamageSync";
+        public const string Version = "0.0.1";
+    }
+
+    [BepInPlugin(Module.GUID, Module.Name, Module.Version)]
+    internal class Entry : BasePlugin
+    {
+        public override void Load()
+        {
+            APILogger.Debug(Module.Name, "Loaded DamageSync");
+            harmony = new Harmony(Module.GUID);
+            harmony.PatchAll();
+        }
+
+        private Harmony harmony;
+    }
+}
---
+++ DamageSync/DamageSync/Patches/DamageSync.cs
@@ -0,0 +1,33 @@
+﻿using API;
+using HarmonyLib;
+
+namespace DamageSync.Patches
+{
+    [HarmonyPatch]
+    internal class DamageSync
+    {
+        [HarmonyPatch(typeof(Dam_EnemyDamageBase), nameof(Dam_EnemyDamageBase.ProcessReceivedDamage))]
+        [HarmonyPostfix]
+        public static void ProcessReceivedDamage(Dam_EnemyDamageBase __instance)
+        {
+            if (!SNetwork.SNet.IsMaster) return;
+
+#if DEBUG
+            APILogger.Debug(Module.Name, $"Sent health value: {__instance.Health}");
+#endif
+            
+            __instance.SendSetHealth(__instance.Health);
+        }
+
+        [HarmonyPatch(typeof(Dam_EnemyDamageBase), nameof(Dam_EnemyDamageBase.ReceiveSetHealth))]
+        [HarmonyPostfix]
+        public static void ReceiveSetHealth(Dam_EnemyDamageBase __instance)
+        {
+            if (SNetwork.SNet.IsMaster) return;
+
+#if DEBUG
+            APILogger.Debug(Module.Name, $"Received health value: {__instance.Health}");
+#endif
+        }
+    }
+}
