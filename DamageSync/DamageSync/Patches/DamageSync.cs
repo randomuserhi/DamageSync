70970b07b6b713e7339d09715a3ec8bef9bcf920
Christopher Teo <40913834+randomuserhi@users.noreply.github.com>
2025-07-10 06:31:44 +0100

    Update DamageSync.cs

--- DamageSync/DamageSync/Patches/DamageSync.cs
+++ DamageSync/DamageSync/Patches/DamageSync.cs
@@ -11,7 +11,22 @@ namespace DamSync {
         [HarmonyPatch(typeof(Dam_EnemyDamageBase), nameof(Dam_EnemyDamageBase.ProcessReceivedDamage))]
         [HarmonyPostfix]
         private static void ProcessReceivedDamage(Dam_EnemyDamageBase __instance, float damage, Agent damageSource, Vector3 position, Vector3 direction, ES_HitreactType hitreact, bool tryForceHitreact, int limbID) {
-            Sync(__instance, limbID);
+            if (!SNet.IsMaster) return;
+
+            // Big thanks to @Dex on GTFO modding discord:
+            // - The below snippet sends a sethleathpacket with a channel type thats more important than what it normally is
+
+            var data = default(pSetHealthData);
+            data.health.Set(__instance.Health, __instance.HealthMax);
+            __instance.m_setHealthPacket.Send(data, SNet_ChannelType.GameReceiveCritical);
+            //line below is potentially useless, but thats what the game does natively in SendSetHealth
+            //__instance.Health = data.health.Get(__instance.HealthMax);
+
+            if (limbID >= 0 && limbID < __instance.DamageLimbs.Count) {
+                Network.SendLimbHealth(__instance.Owner, limbID, __instance.DamageLimbs[limbID].m_health);
+            }
+
+            if (ConfigManager.Debug) APILogger.Debug(Module.Name, $"Sent health value: {__instance.Health}");
         }
 
         [HarmonyPatch(typeof(Dam_EnemyDamageBase), nameof(Dam_EnemyDamageBase.ReceiveSetHealth))]
@@ -29,20 +44,23 @@ namespace DamSync {
         public static void Sync(Dam_EnemyDamageBase __instance, int limbID = -1) {
             if (!SNet.IsMaster) return;
 
-            // Big thanks to @Dex on GTFO modding discord:
-            // - The below snippet sends a sethleathpacket with a channel type thats more important than what it normally is
+            if (limbID >= 0) {
+                if (limbID < __instance.DamageLimbs.Count) {
+                    Network.SendLimbHealth(__instance.Owner, limbID, __instance.DamageLimbs[limbID].m_health);
+                } else {
+                    APILogger.Error(Module.Name, $"Invalid limb: {limbID}. There are only {__instance.DamageLimbs.Count} limbs on this enemy.");
+                }
 
-            var data = default(pSetHealthData);
-            data.health.Set(__instance.Health, __instance.HealthMax);
-            __instance.m_setHealthPacket.Send(data, SNet_ChannelType.GameReceiveCritical);
-            //line below is potentially useless, but thats what the game does natively in SendSetHealth
-            //__instance.Health = data.health.Get(__instance.HealthMax);
+                if (ConfigManager.Debug) APILogger.Debug(Module.Name, $"Sent limb({limbID}) health value: {__instance.DamageLimbs[limbID].m_health}");
+            } else {
+                var data = default(pSetHealthData);
+                data.health.Set(__instance.Health, __instance.HealthMax);
+                __instance.m_setHealthPacket.Send(data, SNet_ChannelType.GameReceiveCritical);
+                //line below is potentially useless, but thats what the game does natively in SendSetHealth
+                //__instance.Health = data.health.Get(__instance.HealthMax);
 
-            if (limbID >= 0 && limbID < __instance.DamageLimbs.Count) {
-                Network.SendLimbHealth(__instance.Owner, limbID, __instance.DamageLimbs[limbID].m_health);
+                if (ConfigManager.Debug) APILogger.Debug(Module.Name, $"Sent health value: {__instance.Health}");
             }
-
-            if (ConfigManager.Debug) APILogger.Debug(Module.Name, $"Sent health value: {__instance.Health}");
         }
     }
 }
